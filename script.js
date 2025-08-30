788c2cca263a089bcd080933dc0275e366c0c6a6
DhruvanshKumar <dhruvansh1309@gmail.com>
2025-08-31 04:58:10 +0530

    Final i guess

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ function downloadResume(){
 function scrollToSection() {
     document.getElementById("about").scrollIntoView({behavior:"smooth"});
 }
+function openWebsite(){
+    const link = document.createElement('a');
+    link.target = "_blank";
+    link.rel = "noopener noreferrer";
+    link.href = 'https://dhruvansh.vercel.app';
+    link.click();
+}
+function openGithub1(){
+    const link = document.createElement('a');
+    link.target = "_blank";
+    link.rel = "noopener noreferrer";
+    link.href = 'https://github.com/DhruvanshKumar/personalPortfoliobootstrap';
+    link.click();
+}
+function openWebsite2(){
+    alert("Oops!! Website under development. You can still view its GitHub");
+}
+function openGithub(){
+    const link = document.createElement('a');
+    link.href = 'https://github.com/DhruvanshKumar/DineOnTime.git';
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer'
+    link.click();
+}
